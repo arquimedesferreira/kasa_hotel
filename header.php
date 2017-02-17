a477848d5800914a5a8d2adb22249c2f77eb089b
ferrerasousa19.88@gmail.com <65528803>
2017-02-17 15:26:29 -0300

    ADD Novos aquivos

--- header.php
+++ header.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+           <meta charset="utf-8">
         <!-- traz o diretorio raiz -->
         <?php $home = get_template_directory_uri();?>
+        
+     
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet" type="text/css" href="<?=$home;?>/style.css">
+
+
         <?php wp_head();?>
         
     </head>
