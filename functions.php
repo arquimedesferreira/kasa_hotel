ebcb82798b24acd4b1749493cad5d736f1a8b28d
arquimedesferreira <ferrerasousa19.88@gmail.com>
2017-02-18 18:06:18 -0300

    add novo arquivos

--- functions.php
+++ functions.php
@@ -2,31 +2,43 @@
 
 add_theme_support( 'post-thumbnails' );
 
-$labes=array(
+
+function mostrar_post_item(){
+
+    $labes=array(
     'name'=>'Itens',
     'name_singular'=>'Item',
     'add_new_item'=> 'Registrar Item',
-    'add_item'=>' Editar Imóvel'
-);
-// Define o que vai ter no tela do novo tipo de post 
-$suports= array(
-    'title',
-    'editor',
-    'thumbnail'
-);
+    'add_item'=>' Editar Item',
+   
+    );
+    // Define o que vai ter no tela do novo tipo de post 
+    $suports= array(
+        'title',
+        'editor',
+        'thumbnail',
+        'page-attributes'
+    );
 
-$args= array(
-    'labels'=>$labes,
-    'public'=>true,
-    'description'=>'Apenas vários itens ',
-    'menu_icon'=>'dashicons-image-filter',
-    'supports'=>$suports
-);
+    $args= array(
+        'labels'=>$labes,
+        'public'=>true,
+        'description'=>'Apenas vários itens ',
+        'menu_icon'=>'dashicons-image-filter',
+        'supports'=>$suports,
+         
+    );
+
+    register_post_type('item',$args);
+}
 
 
 
+function criar_carrocel(){
+    
+}
 
-register_post_type('item',$args);
+add_action('init', 'mostrar_post_item');
 
 
 
--- index.php
+++ index.php
@@ -1,29 +1,82 @@
 <?php get_header();?>
-                          
-         
+<?php $home_index = get_template_directory_uri();?>
 
+ <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a href="#" class="active nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Messages</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>                         
+    
+
+    <!---  Carrocel de imagems  -->
+ <div id="fullcarousel-example" data-interval="false" class="carousel slide"
+    data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="<?=$home_index?>/img/dining-table-1348717_960_720.jpg">
+          <div class="carousel-caption">
+            <h2>Title</h2>
+            <p>Description</p>
+          </div>
+        </div>
+        <div class="item">
+          <img src="<?=$home;?>/img/imagem mesa montada.jpg">
+          <div class="carousel-caption">
+            <h2>Title</h2>
+            <p>Description</p>
+          </div>
+        </div>
+        <div class="item">
+          <img src="<?=$home;?>/img/JPG_9ab6ebc36f346a8fcbf0c8e670c17b69.jpg">
+          <div class="carousel-caption">
+            <h2>Title</h2>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#fullcarousel-example" data-slide="prev"><i class="icon-prev fa fa-angle-left"></i></a>
+      <a class="right carousel-control" href="#fullcarousel-example" data-slide="next"><i class="icon-next fa fa-angle-right"></i></a>
+    </div>
 
 <div class="section">
       <div class="container">
         <!--Inicio do Looop -->
          <?php  
-                if(have_posts()){
-                        while(have_posts()){ 
-                         the_post();
+              $args= array( 'post_type'=>'item' );
+               $loop =new  WP_Query($args);
+                if($loop->have_posts()){
+                        while($loop->have_posts()){ 
+                         $loop->the_post();
         ?>   
-        <div class="row">
-        <!--Coluna  -->
-          <div class="col-md-3">
-            <img class="img-responsive" src=<?php the_post_thumbnail();?>
-            <h2 class="text-center" ><?php the_title(); ?> </h2>
-            <p><?php the_content(); ?>
+        <a href="<?php the_permalink(); ?>">
+            <div class="row">
+            <!--Coluna  -->
+              <div class="col-md-3">
+                <img class="img-responsive" src="<?php the_post_thumbnail_url();?>">
               
-          </div>
-          <?php                  
-                        }
-                }?>
-        </div>
-        
+                <h2 class="text-center" ><?=the_title(); ?> </h2>
+                <p><?=the_content(); ?></p>
+                  
+              </div>
+              <?php                  
+                            }
+                    }?>
+            </div>
+        </a>   
       </div>
     </div>
 <?php get_footer();?>
