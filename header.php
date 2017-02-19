43cd2df95d9ba28c6e30a87925a86198258a9a4f
arquimedes <arquimedes@dilady.com.br>
2017-02-19 19:36:16 -0300

    header

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 <html>
     <head>
            <meta charset="utf-8">
+            <title> 
+         <?php carregaTitulo() ?>
+         </title>
         <!-- traz o diretorio raiz -->
         <?php $home = get_template_directory_uri();?>
         
@@ -9,22 +12,35 @@
         <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
         <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        
+        <!-- CSS sepados para cada item  -->
+        <link rel="stylesheet" href="<?= $home ?>/assets/css/reset.css">
+	      <link rel="stylesheet" href="<?= $home; ?>/assets/css/comum.css">
+	      <link rel="stylesheet" href="<?= $home; ?>/assets/css/header.css">
+
+
         <link rel="stylesheet" type="text/css" href="<?=$home; ?>/style.css">
-         <title> 
-         <?php carregaTitulo() ?>
-         </title>
+
+
+
+        
 
         <?php wp_head();?>
         
     </head>
   <body>
 
+
+
+    
+    <header>
+    <div class="container">
               <?php 
-               $args =array(
-                 'theme_location'=>'header-menus'
-                );
-                 wp_nav_menu($args);
+               $args =array('theme_location'=>'header-menus','show_home'=>'Home');
+               wp_page_menu($args);
               ?>
+      </div>
+    </header
 
 
   <div class="py-5">
